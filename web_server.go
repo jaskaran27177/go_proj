b56a2c768d7dac6120f912b3a93f13c50a705169
jaskaran27177 <jaskarandhanoa7@gmail.com>
2025-02-23 21:10:58 -0800

    made it import all packages inside www folder

--- web_server.go
+++ web_server.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	_ "myproject/www"        // ✅ Import for init() execution only
-	_ "myproject/www/v1/sum" // ✅ Ensure sum init() runs
+	_ "myproject/www" // ✅ Import for init() execution only
 )
 
 
